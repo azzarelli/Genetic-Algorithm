f3c890a90501b2ae44158a2215b9e22444c19ae3
azzadri007 <azzadri007@gmail.com>
2020-06-08 13:32:24 +0100

    final

--- classes.py
+++ classes.py
@@ -5,20 +5,20 @@ class PersonClass(object):
     random.seed(datetime.now())
     
 # Constructor
-    def __init__(self, number, beauty, intelligence, strength): 
+    def __init__(self, number, beauty, intelligence, strength, PTI): 
         randNum = random.random()
         
         if(randNum < 0.01): #   1% Genetic Mutation 
             if(randNum < 0.002):
-                self.beauty = (beauty + random.randint(0,10))%2
+                self.beauty = (beauty + random.randint(0,20))%2
                 self.intelligence = intelligence
                 self.strength = strength
             elif(randNum < 0.006):
-                self.intelligence = (intelligence + random.randint(0,10))%2
+                self.intelligence = (intelligence + random.randint(0,80))%2
                 self.beauty = beauty
                 self.strength = strength
             else:
-                self.strength = (strength + random.randint(0,10))%2
+                self.strength = (strength + random.randint(0,100))%2
                 self.beauty = beauty
                 self.intelligence = intelligence
         else:
@@ -26,8 +26,10 @@ class PersonClass(object):
             self.intelligence = intelligence
             self.strength = strength
 
+        self.prone_to_illness = PTI
+        self.health = 100
         self.fit = strength + beauty + intelligence
         self.surv = float(self.fit/20)
         self.id = number
         self.age = 0
-        self.mated = 0
+        self.children = 0
--- main.py
+++ main.py
@@ -8,76 +8,70 @@
     # Survival rating (/11) = Beauty (2) + Strength (5) + Intelligence(4)
 
 
-import classes
-import random
-from datetime import datetime
+import classes # classes.py contains person class
+import functions # functions.py contains functions used
 
-import plotly.graph_objects as go
+import random # for randomness
+from datetime import datetime # for random seed
 
+import plotly.graph_objects as go # to plot results
 
-# Generate first population (All is random)
-# Qualitites: strength (5), beauty(2), intelligence(4),
-#   returns fitness
-# Changes: Mutation and Parent Cross Over
-
-
+# Set the variables we will use to track the population
 track_size = []
 track_beauty = []
 track_strength = []
 track_intelligence = []
 
-population = []
-random.seed(datetime.now())
-alive = []
-beauty = []
+random.seed(datetime.now()) # Set the seed
 
-for i in range(100):
-    population.append(classes.PersonClass(i, random.randint(0,2),random.randint(0,8),random.randint(0,10)))
+population = [] # Create an array where we will store the individual's info
+alive = [] # Create an array where we store those that survive
+beauty = [] # Create array to store the level of beauty of individuals
 
-alive_num = len(population)
+children_num = 0 # set the id given to a new children to identify it as 0
 
-alive = []
-beauty = []
+for i in range(100): # initialise the first generation
+    population.append(classes.PersonClass(i, random.randint(0,2),random.randint(0,8),random.randint(0,10), 0))
 
-    # who survives in population
+alive_num = len(population) # determine how many are alive
+
+# Who survives the first year
 for obj in population:
     if(obj.surv >= 0.6): # harsh world only 40% of people live
         if(obj.age < 3):
             alive.append(obj)
             beauty.append(obj.beauty)
 
-alive_num = len(alive)
+alive_num = len(alive) # re-calculate number alive
 
-children_num = 0
-
-#   parents make children
+# First Gen reproduction
 for i in range(0, alive_num):
+    
     randNum = random.randint(0,5)
     for j in range(i+1, alive_num):
         if((randNum == 1) and (beauty[i] == beauty[j])): # only 1/5 chance of having children
                                                             # but everyone mates with everyone at same beauty level
-            children_num = children_num + 1
+            children_num = children_num + 1 # adding to child's id
 
-            child_intelligence = int((alive[i].intelligence + alive[j].intelligence)/2)
-            child_strength = int((alive[i].strength + alive[j].strength)/2)
+            child_intelligence = int((alive[i].intelligence + alive[j].intelligence)/2) # determining child's intelligence
+            child_strength = functions.child_str(alive[i].strength, alive[j].strength) # and strenght (in functions.py)
            
-            alive.append(classes.PersonClass(children_num, beauty[i], child_intelligence, child_strength))
+            alive.append(classes.PersonClass(children_num, beauty[i], child_intelligence, child_strength, 0)) # create the child
             
 
-track_size.append(len(alive))
+track_size.append(len(alive)) # track the new # of individuals
 
-# Initial Tracking Values
+# Initialise Tracking Values
 num_str = 0
 num_intel = 0
 num_beau = 0
 
-    
 for obj in population:
     if(obj.beauty == 2):
         num_beau = num_beau + 1
-    if(obj.strength == 5):
+    if(obj.strength == 10):
         num_str = num_str + 1
-    if(obj.intelligence == 4):
+    if(obj.intelligence == 8):
         num_intel = num_intel + 1
 
     
@@ -85,25 +79,41 @@ track_intelligence.append(num_intel)
 track_strength.append(num_str)
 track_beauty.append(num_beau)
 
+# Set the year to 1
 
-while(len(alive) < 1000):
+year = 1
+
+
+while(year < 100):
+
+    random.seed(datetime.now()) # Seed forever changes
 
     # Generate Random Constants to be used
     randNum1 = random.random() # number from 0.0 to 1.0, used for twin, triplet, quadruplet chance ... 
     randNum2 = random.randint(0,2) # number between 0 and 2, set threshold for death due to stupidity
     
-    alive = []
+    alive = [] # re-set alive array
     
     # Determine who survives
     for obj in population:
-        if(obj.surv >= 0.1): # harsh world only 90% of people live
-            if(obj.intelligence > randNum2):
+        if(obj.surv >= 0.6): # harsh world only 40% of people live
+            if(obj.intelligence > randNum2): # death by stupidity
                 if(obj.age < random.randint(75,100)): # people between ages of 75 and 100 are more likely to die
                                                     # thos younger than these random ages are passed on
-                    alive.append(obj)
-                    beauty.append(obj.beauty)
+                    if(obj.health > 0):
+                        alive.append(obj) # track those survived
+                        beauty.append(obj.beauty) # track the 'attraction' of all individuals that have survived
 
-    alive_num = len(alive)
+    alive_num = len(alive) # caluclate new number of people that have survived
+
+    population = alive # re-calculate the new population
+
+    # Determine health of an individual given their conditions
+    for obj in population:
+        obj.health = obj.health - (2 * obj.prone_to_illness)
+        if(obj.age > 80): # old people age badly - sorry 
+            obj.health = obj.health - int(obj.age/80)
+    
 
     # Determine who has children && have them
     for i in range(0, alive_num):
@@ -114,48 +124,54 @@ while(len(alive) < 1000):
 
                 if((alive[i].age > 25)and(alive[j].age>25)and(alive[i].age<55)and(alive[j].age<55)): # age of reproduction becomes a factor
 
-                    if((alive[i].mated == 0)and(alive[j].mated == 0)): # only mates with someone once a year
-                        child_intelligence = int((alive[i].intelligence + alive[j].intelligence)/2)
-                        child_strength = int((alive[i].strength + alive[j].strength)/2)
+                    if((alive[i].children < 5)and(alive[j].children < 5)): # only mates with someone once a year
+                        child_intelligence = functions.child_intel(alive[i].intelligence, alive[j].intelligence)
+
+                        child_strength = functions.child_str(alive[i].strength, alive[j].strength)
 
                         # Likelihood of having more than one child at once
-                        if(randNum1 < 0.004): # 0.40% of having twins
-                            alive.append(classes.PersonClass(children_num, beauty[i], child_intelligence, child_strength))
-                            alive.append(classes.PersonClass(children_num+1, beauty[i], child_intelligence, child_strength))
+                        if(randNum1 < 0.004): # 0.40% of having twins and set them
+                            alive.append(classes.PersonClass(children_num, beauty[i], child_intelligence, child_strength, 0))
+                            alive.append(classes.PersonClass(children_num+1, beauty[i], child_intelligence, child_strength, 0))
                             children_num = children_num + 2
-                        elif(randNum1 < 0.00412): # 0.012% of having triplets
-                            alive.append(classes.PersonClass(children_num, beauty[i], child_intelligence, child_strength))
-                            alive.append(classes.PersonClass(children_num+1, beauty[i], child_intelligence, child_strength))
-                            alive.append(classes.PersonClass(children_num+2, beauty[i], child_intelligence, child_strength))
+                            alive[i].children = alive[i].children + 2
+                            alive[j].children = alive[j].children + 2
+                        elif(randNum1 < 0.00412): # 0.012% of having triplets and set them
+                            alive.append(classes.PersonClass(children_num, beauty[i], child_intelligence, child_strength, 0))
+                            alive.append(classes.PersonClass(children_num+1, beauty[i], child_intelligence, child_strength, 0))
+                            alive.append(classes.PersonClass(children_num+2, beauty[i], child_intelligence, child_strength, 0))
                             children_num = children_num + 3
-                        else: # Otherwise have my own
-                            alive.append(classes.PersonClass(children_num, beauty[i], child_intelligence, child_strength))
+                            alive[i].children = alive[i].children + 3
+                            alive[j].children = alive[j].children + 3
+                        else: # otherwise have one
+                            alive.append(classes.PersonClass(children_num, beauty[i], child_intelligence, child_strength, 0))
                             children_num = children_num + 1
-                        
-                        alive[i].mated = alive[i].mated + 1
-                        alive[j].mated = alive[j].mated + 1
+                            alive[i].children = alive[i].children + 1
+                            alive[j].children = alive[j].children + 1
 
-
-    population = alive
-    track_size.append(len(alive))
+    population = alive # set the new population to account for new borns
 
     # age everyone and return mated count to 0
     for obj in population:
         obj.age = obj.age + 1
-        obj.mated = 0
+
+    # Reset the array that tracks beauty as those that survives forever changes
+    beauty = []
 
     # Calculate Tracking Values
-    num_str = 0
+
+    num_str = 0 # reset tracking values to 0 
     num_intel = 0
     num_beau = 0
 
-    
-    for obj in population:
+    track_size.append(len(population)) # track the population at the end of the year
+
+    for obj in population: # track the number of individuals who are at max strength or intelligence or beauty
         if(obj.beauty == 2):
             num_beau = num_beau + 1
-        if(obj.strength == 5):
+        if(obj.strength == 10):
             num_str = num_str + 1
-        if(obj.intelligence == 4):
+        if(obj.intelligence == 8):
             num_intel = num_intel + 1
 
     
@@ -163,8 +179,13 @@ while(len(alive) < 1000):
     track_strength.append(num_str)
     track_beauty.append(num_beau)
 
-    beauty = []
+    
+    print(year)
+    year = year + 1 # year increases
 
+
+
+# Graphing #################################################################################################################################################################
 
 step = []
 
@@ -172,7 +193,6 @@ for i in range(len(track_size)):
     print(track_size[i], ' ', track_beauty[i], ' ', track_intelligence[i], ' ', track_strength[i])
 
     step.append(i)
-
 
 # Graphing Outcome
 
